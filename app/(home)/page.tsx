e5992edc6b8fcba42b5b0e710f2856f3124c84c2
leander <foolsparadise984@gmail.com>
2025-01-25 22:06:52 +0800

    clean

--- app/(home)/page.tsx
+++
@@ -1,9 +0,0 @@
-import Hero from '@/components/hero';
-
-export default function HomePage() {
-  return (
-    <main className="container relative max-w-[1100px] px-2 py-4 lg:py-16">
-      <Hero />
-    </main>
-  );
-}
